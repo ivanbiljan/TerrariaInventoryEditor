bd8dda8d5df97b94ad83530a09db1e491d2c46be
ivanbiljan <ssbiljan@gmail.com>
2021-04-04 14:10:56 +0200

    Add ViewModelBase<T>

---
+++ TerrariaKit/ViewModels/ViewModelBase{T}.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using TerrariaKit.Annotations;
+
+namespace TerrariaKit.ViewModels
+{
+    /// <summary>
+    /// Represents the base class for a ViewModel.
+    /// </summary>
+    /// <typeparam name="T">The type of model this ViewModel encapsulates.</typeparam>
+    internal sealed class ViewModelBase<T> : INotifyPropertyChanged where T : class
+    {
+        private readonly IDictionary<string, object?> _propertyNameToValueMapping = new Dictionary<string, object?>();
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public T Get<T>([CallerMemberName] string? name = null)
+        {
+            return (_propertyNameToValueMapping.TryGetValue(name!, out var obj) && obj != null ? (T) obj : default)!;
+        }
+
+        public void Set<T>(T value, [CallerMemberName] string? name = null)
+        {
+            if (EqualityComparer<T>.Default.Equals(Get<T>(name), value))
+            {
+                return;
+            }
+
+            _propertyNameToValueMapping[name!] = value;
+            OnPropertyChanged(name!);
+        }
+
+        [NotifyPropertyChangedInvocator]
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
